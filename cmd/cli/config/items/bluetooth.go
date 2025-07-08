2f871056928f144a646c2ce38ddc38f290a8cd6f
Gavin Ho <gavintyho@gmail.com>
2025-07-08 15:43:29 -0700

    added bluetooth indicator

---
+++ cmd/cli/config/items/bluetooth.go
@@ -0,0 +1,123 @@
+package items
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"github.com/lucax88x/wentsketchy/cmd/cli/config/args"
+	"github.com/lucax88x/wentsketchy/cmd/cli/config/settings"
+	"github.com/lucax88x/wentsketchy/cmd/cli/config/settings/colors"
+	"github.com/lucax88x/wentsketchy/cmd/cli/config/settings/icons"
+	"github.com/lucax88x/wentsketchy/internal/command"
+	"github.com/lucax88x/wentsketchy/internal/sketchybar"
+	"github.com/lucax88x/wentsketchy/internal/sketchybar/events"
+)
+
+type BluetoothItem struct {
+	logger  *slog.Logger
+	command *command.Command
+}
+
+func NewBluetoothItem(logger *slog.Logger, command *command.Command) BluetoothItem {
+	return BluetoothItem{logger, command}
+}
+
+const bluetoothItemName = "bluetooth"
+
+func (i BluetoothItem) Init(
+	_ context.Context,
+	position sketchybar.Position,
+	batches Batches,
+) (Batches, error) {
+	updateEvent, err := args.BuildEvent()
+	if err != nil {
+		return batches, errors.New("bluetooth: could not generate update event")
+	}
+
+	bluetoothItem := sketchybar.ItemOptions{
+		Display: "active",
+		Padding: sketchybar.PaddingOptions{
+			Left:  settings.Sketchybar.ItemSpacing,
+			Right: settings.Sketchybar.ItemSpacing,
+		},
+		Icon: sketchybar.ItemIconOptions{
+			Value: icons.Bluetooth,
+			Font: sketchybar.FontOptions{
+				Font: settings.FontIcon,
+			},
+			Padding: sketchybar.PaddingOptions{
+				Left:  settings.Sketchybar.IconPadding,
+				Right: pointer(*settings.Sketchybar.IconPadding / 2),
+			},
+		},
+		Label: sketchybar.ItemLabelOptions{
+			Padding: sketchybar.PaddingOptions{
+				Left:  pointer(0),
+				Right: settings.Sketchybar.IconPadding,
+			},
+		},
+		UpdateFreq: pointer(120),
+		Updates:    "on",
+		Script:     updateEvent,
+	}
+
+	batches = batch(batches, s("--add", "item", bluetoothItemName, position))
+	batches = batch(batches, m(s("--set", bluetoothItemName), bluetoothItem.ToArgs()))
+	batches = batch(batches, s("--add", "event", "bluetooth_change"))
+	batches = batch(batches, s("--subscribe", bluetoothItemName, events.SystemWoke, "bluetooth_change"))
+
+	return batches, nil
+}
+
+func (i BluetoothItem) Update(
+	ctx context.Context,
+	batches Batches,
+	_ sketchybar.Position,
+	args *args.In,
+) (Batches, error) {
+	if !isBluetooth(args.Name) {
+		return batches, nil
+	}
+
+		if args.Event == events.Routine || args.Event == events.Forced || args.Event == events.SystemWoke || args.Event == "bluetooth_change" {
+		output, err := i.command.Run(ctx, "blueutil", "-p")
+		if err != nil {
+			return batches, fmt.Errorf("bluetooth: could not get bluetooth status. %w", err)
+		}
+
+		trimmedOutput := strings.TrimSpace(output)
+		var label, color string
+
+		if trimmedOutput == "1" {
+			label = "Bluetooth On"
+			color = colors.Blue
+		} else {
+			label = "Bluetooth Off"
+			color = colors.White
+		}
+
+		bluetoothItem := sketchybar.ItemOptions{
+			Icon: sketchybar.ItemIconOptions{
+				Color: sketchybar.ColorOptions{
+					Color: color,
+				},
+			},
+			Label: sketchybar.ItemLabelOptions{
+				Value: label,
+			},
+		}
+
+		batches = batch(batches, m(s("--set", bluetoothItemName), bluetoothItem.ToArgs()))
+	}
+
+	return batches, nil
+}
+
+func isBluetooth(name string) bool {
+	return name == bluetoothItemName
+}
+
+var _ WentsketchyItem = (*BluetoothItem)(nil)
---
+++ cmd/cli/config/items/bluetooth_job.go
@@ -0,0 +1,63 @@
+package items
+
+import (
+	"context"
+	"log/slog"
+	"strings"
+	"time"
+
+	"github.com/lucax88x/wentsketchy/internal/command"
+	"github.com/lucax88x/wentsketchy/internal/sketchybar"
+)
+
+type BluetoothJob struct {
+	logger     *slog.Logger
+	command    *command.Command
+	sketchybar sketchybar.API
+}
+
+func NewBluetoothJob(logger *slog.Logger, command *command.Command, sketchybar sketchybar.API) *BluetoothJob {
+	return &BluetoothJob{logger, command, sketchybar}
+}
+
+func (j *BluetoothJob) Start(ctx context.Context) {
+	go func() {
+		var lastStatus string
+		ticker := time.NewTicker(2 * time.Second) // Check every 2 seconds
+		defer ticker.Stop()
+
+		// Initial check
+		output, err := j.command.Run(ctx, "blueutil", "-p")
+		if err != nil {
+			j.logger.Error("bluetooth job: could not get initial bluetooth status", "error", err)
+		}
+		lastStatus = strings.TrimSpace(output)
+		// Trigger a refresh on start, so the label is correct
+		err = j.sketchybar.Run(ctx, []string{"--trigger", "bluetooth_change"})
+		if err != nil {
+			j.logger.Error("bluetooth job: could not trigger initial event", "error", err)
+		}
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				output, err := j.command.Run(ctx, "blueutil", "-p")
+				if err != nil {
+					j.logger.Error("bluetooth job: could not get bluetooth status", "error", err)
+					continue
+				}
+
+				currentStatus := strings.TrimSpace(output)
+				if currentStatus != lastStatus {
+					err := j.sketchybar.Run(ctx, []string{"--trigger", "bluetooth_change"})
+					if err != nil {
+						j.logger.Error("bluetooth job: could not trigger event", "error", err)
+					}
+				}
+				lastStatus = currentStatus
+			}
+		}
+	}()
+}
